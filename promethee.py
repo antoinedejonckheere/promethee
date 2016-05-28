cfdf23d16c2a6a94bd037626e45d0fc811de57f0
antoinedejonckheere <antoine.dejonckheere@realimpactanalytics.com>
2016-05-28 14:24:45 +0200

    ADDED promethee I ranking matrix computation

--- promethee.py
+++ promethee.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import utils
 from preference_functions import * 
 
@@ -17,6 +18,9 @@ class Promethee:
 		self.computeNegativeFlow();
 		self.computeGlobalFlow();
 		self.getPrometheeIIRanking();
+		self.computePositiveMatrix();
+		self.computeNegativeMatrix();
+		self.computePrometheeIMatrix();
 	def normalizeWeights(self):
 		# normalizes the weights given as an input;
 		sumW = sum(self.weights);
@@ -72,3 +76,62 @@ class Promethee:
 					self.pi[a][b] = sum;
 		print("Pairwise comparison matrix is")
 		print self.pi;
+	def computePositiveMatrix(self):
+		self.positive_matrix = utils.initialise_matrix(self.numberAlternatives,self.numberAlternatives);
+		for a in xrange(self.numberAlternatives):
+			for b in xrange(self.numberAlternatives):
+				if self.phi_plus[a]>self.phi_plus[b]:
+					self.positive_matrix[a][b] = 1;
+				elif self.phi_plus[a]<self.phi_plus[b]:
+					self.positive_matrix[a][b] = -1;
+				else:
+					self.positive_matrix[a][b] = 0;
+		for a in xrange(self.numberAlternatives):
+			for b in xrange(self.numberAlternatives):
+				print("Alternative %s with positive flow %s \n") %(a,self.phi_plus[a]);
+				print("Alternative %s with positive flow %s \n") %(b,self.phi_plus[b]);
+				print("making positive matrix element of %s \n") %(self.positive_matrix[a][b]);
+				print("########################################");
+	def computeNegativeMatrix(self):
+		self.negative_matrix = utils.initialise_matrix(self.numberAlternatives,self.numberAlternatives);
+		for a in xrange(self.numberAlternatives):
+			for b in xrange(self.numberAlternatives):
+				if self.phi_minus[a]<self.phi_minus[b]:
+					self.negative_matrix[a][b] = 1;
+				elif self.phi_minus[a]>self.phi_minus[b]:
+					self.negative_matrix[a][b] = -1;
+				else:
+					self.negative_matrix[a][b] = 0;
+		for a in xrange(self.numberAlternatives):
+			for b in xrange(self.numberAlternatives):
+				print("Alternative %s with negative flow %s \n") %(a,self.phi_minus[a]);
+				print("Alternative %s with negative flow %s \n") %(b,self.phi_minus[b]);
+				print("making negative matrix element of %s \n") %(self.negative_matrix[a][b]);
+				print("########################################");
+	def computePrometheeIMatrix(self):
+		self.prometheeI_matrix = utils.initialise_matrix(self.numberAlternatives,self.numberAlternatives);
+		for a in range(self.numberAlternatives):
+			for b in range(self.numberAlternatives):
+				# cas 1 : les deux flux donnent le même résultat
+				# en conséquence, le ranking promethee I est équivalent au ranking de chacun des flux positif
+				# et négatif
+				# ceci couvre les cas : double préférence, double indifférence
+				if (self.positive_matrix[a][b] == self.negative_matrix[a][b]):
+					self.prometheeI_matrix[a][b] = self.positive_matrix[a][b]
+				# cas 2: on doit gérer la présence d'une indifférence
+				else:
+					# cas 1: l'un des deux classements donne une égalité.
+					# dans ce cas, le ranking est la somme des deux rankings
+					if (self.positive_matrix[a][b]==0 or self.negative_matrix[a][b]==0):
+						self.prometheeI_matrix[a][b]= self.positive_matrix[a][b]+self.negative_matrix[a][b];
+					# dernier cas: les deux resultats sont différents et aucun n'est nul. Donc, incomparabilité!
+					else:
+						self.prometheeI_matrix[a][b] = "R";
+		for a in range(self.numberAlternatives):
+			for b in range(self.numberAlternatives):
+				print('#################################\n');
+				print('Element %s,%s: \n') %(a,b);
+				print('Positive matrix is %s \n') %(self.positive_matrix[a][b]);
+				print('Negative matrix is %s \n') %(self.negative_matrix[a][b]);
+				print('Promethee I ranking matrix is %s \n') %(self.prometheeI_matrix[a][b]);
+				print('#################################\n');
