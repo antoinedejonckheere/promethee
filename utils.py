5a1d832fb1f61de8361aa799ece9106f09af3bbc
antoinedejonckheere <antoine.dejonckheere@realimpactanalytics.com>
2016-05-30 10:21:00 +0200

    added method to initialize random 1D array

--- utils.py
+++ utils.py
@@ -8,8 +8,12 @@ def random_matrix(width, height):
 	matrix = [[random.random() for x in range(width)] for y in range(height)];
 	return matrix;
 
-def random_normalized_array(length):
+def random_array(length):
 	array = [random.random() for x in range(length)];
+	return array
+
+def random_normalized_array(length):
+	array = random_array(length);
 	arraySum = sum(array);
 	for x in range(length):
 		array[x] = array[x]/arraySum;
