e6e74f64885ed320ece875393b45ed5c0d2d1971
antoinedejonckheere <antoine.dejonckheere@realimpactanalytics.com>
2016-06-19 12:15:37 +0200

    Compute and print promethee I and II rank reversal frequency

--- simulation.py
+++ simulation.py
@@ -8,7 +8,10 @@ number_alternatives = 3;
 number_criteria = 2;
 
 # number of simulations to be run
-number_simulations = 10000;
+number_simulations = 1000;
+
+# number of decimals for RR frequency
+ndigits = 5;
 
 # initializes results array
 promethee_I_rr = utils.initialize_array(number_simulations);
@@ -27,3 +30,11 @@ print promethee_I_rr;
 print promethee_II_rr;
 
 print(promethee_I_rr == promethee_II_rr);
+
+## RR frequency for promethee I
+promethee_I_rr_frequency = utils.divide_integers(sum(promethee_I_rr), len(promethee_I_rr));
+print promethee_I_rr_frequency;
+
+## RR frequency for promethee II
+promethee_II_rr_frequency = utils.divide_integers(sum(promethee_II_rr), len(promethee_II_rr));
+print promethee_II_rr_frequency;
