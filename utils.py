3b88d764b2cb7c31064b62a1808cf429225d8f09
antoinedejonckheere <antoine.dejonckheere@realimpactanalytics.com>
2016-06-24 12:59:31 +0200

    preliminary work

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import random;
+import csv;
 
 def initialise_matrix(width,height):
 	Matrix=[[0 for x in range(width)] for y in range(height)];
@@ -24,3 +25,14 @@ def random_normalized_array(length):
 ## returns the result of the division of one integer by another integer
 def divide_integers(numerator, denumerator):
 	return float(numerator)/float(denumerator);
+
+def write_table_to_csv(array,path_to_csv):
+	with open(path_to_csv, 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		[writer.writerow(r) for r in array]
+
+def read_table_from_csv(path_to_csv):
+	with open(path_to_csv, 'r') as csvfile:
+		reader = csv.reader(csvfile)
+		table = [[e for e in r] for r in reader]
+	return table;
