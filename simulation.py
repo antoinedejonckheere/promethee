375e8f2e8b2f490908b5d56e65d95942e5c42ab9
antoinedejonckheere <antoine.dejonckheere@realimpactanalytics.com>
2016-06-25 11:18:47 +0200

    Looping over the different preference functions

--- simulation.py
+++ simulation.py
@@ -7,39 +7,53 @@ from promethee_simulation import prometheeSimulation
 number_alternatives = 3;
 number_criteria = 3;
 
+# preference functions to be considered
+preference_functions = [
+'linear',
+'gaussian',
+'quasi-criterion'];
+
 # number of simulations to be run
-number_simulations = 10000;
+number_simulations = 1000;
 
 # number of decimals for RR frequency
 ndigits = 5;
 
-# initializes results array
-promethee_I_rr = utils.initialize_array(number_simulations);
-promethee_II_rr = utils.initialize_array(number_simulations);
 
-for simulation_number in range(number_simulations):
-	promethee_simu = prometheeSimulation(number_alternatives, number_criteria,'quasi-criterion');
-	promethee_I_rr[simulation_number] = promethee_simu.detect_promethee_I_rr();
-	promethee_II_rr[simulation_number] = promethee_simu.detect_promethee_II_rr();
+results_array = [];
+results_array.append([
+	'preference_function',
+	'promethee I rr count',
+	'frequency of promethee I',
+	 'promethee II rr count',
+	'frequency of promethee II']);
 
+for preference_function in preference_functions:
+	# initializes results array
+	promethee_I_rr = utils.initialize_array(number_simulations);
+	promethee_II_rr = utils.initialize_array(number_simulations);
+	print preference_function;
+	for simulation_number in range(number_simulations):
+		promethee_simu = prometheeSimulation(number_alternatives, number_criteria, 'quasi-criterion');
+		promethee_I_rr[simulation_number] = promethee_simu.detect_promethee_I_rr();
+		promethee_II_rr[simulation_number] = promethee_simu.detect_promethee_II_rr();
+	## RR frequency for promethee I
+	promethee_I_rr_frequency = utils.divide_integers(sum(promethee_I_rr), len(promethee_I_rr));
+	print("number of ranks reversals for promethee I:")
+	print(sum(promethee_I_rr));
+	print("Promethee I rr frequency:");
+	print promethee_I_rr_frequency;
+	## RR frequency for promethee II
+	promethee_II_rr_frequency = utils.divide_integers(sum(promethee_II_rr), len(promethee_II_rr));
+	print("number of ranks reversals for promethee II:")
+	print(sum(promethee_II_rr));
+	print("Promethee II rr frequency:");
+	print promethee_II_rr_frequency;
+	results_array.append([
+		preference_function, 
+		sum(promethee_I_rr), 
+		promethee_I_rr_frequency, 
+		sum(promethee_II_rr), 
+		promethee_II_rr_frequency]);
 
-print promethee_I_rr;
-
-print promethee_II_rr;
-
-print(promethee_I_rr == promethee_II_rr);
-
-## RR frequency for promethee I
-promethee_I_rr_frequency = utils.divide_integers(sum(promethee_I_rr), len(promethee_I_rr));
-print("number of ranks reversals for promethee I:")
-print(sum(promethee_I_rr));
-print("Promethee I rr frequency:");
-print promethee_I_rr_frequency;
-
-## RR frequency for promethee II
-
-promethee_II_rr_frequency = utils.divide_integers(sum(promethee_II_rr), len(promethee_II_rr));
-print("number of ranks reversals for promethee II:")
-print(sum(promethee_II_rr));
-print("Promethee II rr frequency:");
-print promethee_II_rr_frequency;
+print results_array;
