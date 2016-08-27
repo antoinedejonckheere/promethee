fa61ba8a9e21d96f8fbcfaacc62cf3c0bf907a01
antoine dejonckheere <antoinedejonckheere@antoines-mbp.home>
2016-08-27 13:22:17 +0200

    print promethee I matrices and make sure we keep only the cases for changes in winning alternative

--- find_rr.py
+++ find_rr.py
@@ -13,6 +13,9 @@ number_criteria = 2;
 number_alternatives = 3;
 MAX_NUMBER_ITERATIONS = 1000000000000;
 
+winning_alternative_3 = 0;
+winning_alternative_4 = 0;
+
 #weights = [0.5, 0.5];
 preference_function = 'gaussian';
 preference_function_parameters = {'p': 0, 'q': 0, 'sigma': 1};
@@ -20,42 +23,55 @@ preference_function_parameters = {'p': 0, 'q': 0, 'sigma': 1};
 continue_loop = True;
 iterations_count = 0;
 
-while continue_loop:
-    iterations_count +=1;
-    # define the experiment here
-    promethee_simu = prometheeSimulation(number_alternatives, number_criteria, preference_function, preference_function_parameters);
-    conditions_met = (promethee_simu.detect_promethee_I_rr() == 1 and 
-                      promethee_simu.detect_promethee_II_rr()==1 and
-                      promethee_simu.isLastAlternativeOutranked()) or iterations_count > MAX_NUMBER_ITERATIONS;
-    if (conditions_met):
-        continue_loop = False;
+while winning_alternative_3 == winning_alternative_4:
+    while continue_loop:
+        iterations_count +=1;
+        # define the experiment here
+        promethee_simu = prometheeSimulation(number_alternatives, number_criteria, preference_function, preference_function_parameters);
+        conditions_met = (promethee_simu.detect_promethee_I_rr() == 1 and 
+                        promethee_simu.detect_promethee_II_rr()==1 and
+                        promethee_simu.isLastAlternativeOutranked()) or iterations_count > MAX_NUMBER_ITERATIONS;
+        if (conditions_met):
+            continue_loop = False;
 
-print("Weights are");
-print(promethee_simu.get_weights());
-print("Evaluation table is");
-print(promethee_simu.get_evaluation_table());
-print("preference_function is");
-print preference_function;
-print("preference functions parameters are");
-print preference_function_parameters;
-print ('escaping after %s iterations')%iterations_count;
+    print("Weights are");
+    print(promethee_simu.get_weights());
+    print("Evaluation table is");
+    print(promethee_simu.get_evaluation_table());
+    print("preference_function is");
+    print preference_function;
+    print("preference functions parameters are");
+    print preference_function_parameters;
+    print ('escaping after %s iterations')%iterations_count;
 
-print('running promethee methods for the set defined');
-evaluation_table = promethee_simu.get_evaluation_table();
-weights = promethee_simu.get_weights();
-promethee_object = Promethee(evaluation_table, weights, preference_function,preference_function_parameters);
-global_flow = promethee_object.getGlobalFlow();
-for elem in global_flow:
-    print elem;
-print global_flow.index(max(global_flow));
+    print('running promethee methods for the set defined');
+    evaluation_table = promethee_simu.get_evaluation_table();
+    weights = promethee_simu.get_weights();
+    promethee_object = Promethee(evaluation_table, weights, preference_function,preference_function_parameters);
+    global_flow = promethee_object.getGlobalFlow();
 
-promethee_object_removed = Promethee(evaluation_table[0:len(evaluation_table)-1],weights,preference_function,preference_function_parameters)
-global_flow_removed = promethee_object_removed.getGlobalFlow();
-print('######');
-for elem in global_flow_removed:
-    print elem;
-print global_flow_removed.index(max(global_flow_removed));
+    print('PROMETHEE II results');
+    for elem in global_flow:
+        print elem;
+    winning_alternative_4 = global_flow.index(max(global_flow));
+    print winning_alternative_4;
 
+    promethee_object_removed = Promethee(evaluation_table[0:len(evaluation_table)-1],weights,preference_function,preference_function_parameters)
+    global_flow_removed = promethee_object_removed.getGlobalFlow();
+    print('######');
+    for elem in global_flow_removed:
+        print elem;
+    winning_alternative_3 = global_flow_removed.index(max(global_flow_removed)); 
+    print winning_alternative_3;
+
+    print('#########################');
+    print('PROMETHEE I results:')
+    print('4 alternatives:');
+    print promethee_object.getPrometheeIMatrix();
+    print('3 alternatives: ')
+    print promethee_object_removed.getPrometheeIMatrix();
+    if winning_alternative_3==winning_alternative_4:
+        continue_loop=True;
 
 ##Weights are
 ##[0.5317978414431725, 0.4682021585568274]
